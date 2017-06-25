602508e7fdd20129e8096149ad6e957af2302ced
浩王 <haoye@haowangdeiMac.lan>
2017-06-25 11:14:21 +0800

    My

---
+++ app/Http/Controllers/My/ArticlesController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\My;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ArticlesController extends Controller
+{
+    //
+}
--- routes/web.php
+++ routes/web.php
@@ -12,15 +12,14 @@
 */
 //顶层
 Route::group([], function () {
-    Route::resource('/', 'HomeController', ['only' => ['index']]);
-    Route::resource('tags', 'TagsController');
-    Route::resource('articles', 'ArticlesController');
-    Route::resource('celebrities', 'CelebritiesController');
+	Route::resource('/', 'HomeController', ['only' => ['index']]);
+	Route::resource('tags', 'TagsController');
+	Route::resource('articles', 'ArticlesController', ['only' => ['index', 'show']]);
+	Route::resource('celebrities', 'CelebritiesController');
 });
 
-Route::group(['middleware' => 'auth'],function (){
+Route::group(['middleware' => 'auth'], function () {
 
 });
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
